40198647976253c87c0f7dc7842076072ebf5f7e
Michael Treml <miketreml@mac.com>
2023-06-30 16:03:15 -0500

    Update test.ps1

--- test.ps1
+++ test.ps1
@@ -1,5 +1,5 @@
-$User = { "MichaelTreml\r", "Admin678cfcbe0c" }
-$Address = { "LOCAL", "75.7.22.205", "67.175.9.65", "67.253.57.135" }
+$User = "MichaelTreml", "Admin678cfcbe0c" 
+$Address = "LOCAL", "75.7.22.205", "67.175.9.65", "67.253.57.135" 
 [PSCustomObject]@{
 BlackListUser = $User 
 BlackListAddress = $Address
