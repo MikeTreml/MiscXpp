87a509868471e2d857d0a2fa75a4eab1c0f64a59
Michael Treml <miketreml@mac.com>
2023-06-30 16:44:25 -0500

    Update test.ps1

--- test.ps1
+++ test.ps1
@@ -1,5 +1,5 @@
 [PSCustomObject]@{
-BlackListUser = "MichaelTreml Admin678cfcbe0c" 
-BlackListAddress = "LOCAL 75.7.22.205 67.175.9.65 67.253.57.135" 
+BlackListUser = "MichaelTreml, Admin678cfcbe0c" 
+BlackListAddress = "LOCAL, 75.7.22.205, 67.175.9.65, 67.253.57.135" 
 true = "True"
 }
